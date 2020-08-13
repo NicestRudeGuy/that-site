0fcbc2e752d48a07a835bffa330c5bbe2238278c
NicestRudeGuy <vipinkumard365@gmail.com>
2020-08-13 11:01:10 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Service from "./Components/Service";
 import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
-      <About />
-      <Service />
-      <Contact />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/service" component={Service} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
     </>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <div className="container-fluid col-10 mx-auto">
+          <NavLink className="navbar-brand" to="/">
+            Business-Name
+          </NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink activeClassName="activeLink" exact className="nav-link " aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink activeClassName="activeLink" exact className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink activeClassName="activeLink" exact className="nav-link" to="/service">
+                  Service
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink activeClassName="activeLink" exact className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
